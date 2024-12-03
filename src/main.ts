01c0967694964239f1615b0a0020435241003094
Fernando Santos <fern@ndo.io>
2024-12-03 11:46:47 -0500

    Refactoring to Remove Animation & Create a Static Version

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ declare const window: any;
 
 import "./style.scss";
 import { logger } from "./lib/logger";
-import { MonthlySeal } from "./lib/monthly-seal";
+// import { MonthlySeal } from "./lib/monthly-seal";
 import { MonthlyNudgeBalloon } from "./lib/monthly-nudge-balloon";
 import { AmountsCurrency } from "./lib/amounts-currency";
 
@@ -10,11 +10,6 @@ function run() {
   logger("4Site Init");
   new MonthlyNudgeBalloon();
   new AmountsCurrency();
-  const isMonthly = () => {
-    return !!document.querySelector(
-      "input[name='transaction.recurrpay'][value='Y']:checked"
-    );
-  };
   const freqRadios = document.querySelectorAll(
     "input[name='transaction.recurrpay']"
   );
@@ -31,122 +26,21 @@ function run() {
   // Hardcode Give Monthly Label
   if (monthlyLabel) {
     monthlyLabel.innerHTML = `
-    <lottie-player
-      id="monthlyNudgeAnimation"
-      src="https://resources.peta.org/engaging-networks/pages/4site/monthly-nudge/monthly-animation.json"
-      background="transparent"
-      speed="1"
-      style="width: 222px; height: 95px"
-      direction="1"
-      mode="normal"
-    ></lottie-player>
     <span>Monthly</span>
     `;
   }
-  const player = document.querySelector("lottie-player");
-
-  if (player && freqRadios.length && monthlyLabel) {
-    player.addEventListener("load", () => {
-      logger("Lottie Player Loaded");
-      const container = document.querySelector(".peta-monthly-nudge");
-      if (container) container.classList.add("loaded");
-      const lottie = (player as any).getLottie();
-      let isHovering = false;
-      const cleanPaws = () => {
-        if (!isMonthly() && !isHovering) lottie.playSegments([282, 352], false);
-      };
-      // Default state
-      if (isMonthly()) {
-        lottie.playSegments([101, 165], true);
-      } else {
-        lottie.playSegments([0, 42], true);
-      }
-
-      const isReduced =
-        window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
-        window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
-
-      if (!!isReduced) {
-        // DON'T use an amination here!
-        freqRadios.forEach((radio) => {
-          radio.addEventListener("change", (e) => {
-            const target = e.target as HTMLInputElement;
-            toggleInputActivation();
-            if (target.value === "Y") {
-              monthlyLabel.classList.add("active");
-              lottie.playSegments([42, 43], true);
-            } else {
-              monthlyLabel.classList.remove("active");
-              lottie.playSegments([[41, 42]], false);
-              removeMonthlyLabelFromAmounts();
-            }
-          });
-        });
-        if (monthlyLabel) {
-          monthlyLabel.addEventListener("mouseenter", () => {
-            isHovering = true;
-            if (!isMonthly()) lottie.playSegments([42, 43], true);
-          });
-          monthlyLabel.addEventListener("mouseleave", () => {
-            isHovering = false;
-            if (!isMonthly()) lottie.playSegments([41, 42], true);
-          });
-          lottie.onEnterFrame = (e: any) => {
-            if (e.currentTime === 63) {
-              monthlyLabel.classList.add("active");
-            }
-            if (e.totalTime === 1) {
-              monthlyLabel.classList.remove("active");
-              lottie.setSpeed(1);
-            }
-          };
+  if (freqRadios.length && monthlyLabel) {
+    freqRadios.forEach((radio) => {
+      radio.addEventListener("change", (e) => {
+        const target = e.target as HTMLInputElement;
+        toggleInputActivation();
+        if (target.value === "Y") {
+          monthlyLabel.classList.add("active");
+        } else {
+          monthlyLabel.classList.remove("active");
+          removeMonthlyLabelFromAmounts();
         }
-      } else {
-        // DO use an animation here!
-        freqRadios.forEach((radio) => {
-          radio.addEventListener("change", (e) => {
-            const target = e.target as HTMLInputElement;
-            toggleInputActivation();
-            if (target.value === "Y") {
-              monthlyLabel.classList.add("active");
-              lottie.playSegments([101, 165], true);
-            } else {
-              lottie.setSpeed(1.3);
-              lottie.playSegments(
-                [
-                  [166, 222],
-                  [41, 42],
-                ],
-                false
-              );
-              removeMonthlyLabelFromAmounts();
-            }
-          });
-        });
-
-        if (monthlyLabel) {
-          monthlyLabel.addEventListener("mouseenter", () => {
-            isHovering = true;
-            if (!isMonthly()) lottie.playSegments([42, 100], true);
-          });
-          monthlyLabel.addEventListener("mouseleave", () => {
-            isHovering = false;
-            if (!isMonthly()) lottie.playSegments([41, 42], true);
-          });
-          lottie.onEnterFrame = (e: any) => {
-            // console.log(e);
-            if (e.currentTime === 63) {
-              monthlyLabel.classList.add("active");
-            }
-            if (e.totalTime === 1) {
-              monthlyLabel.classList.remove("active");
-              lottie.setSpeed(1);
-            }
-          };
-        }
-        // Clean Paws Every 5 seconds
-        setInterval(cleanPaws, 5000);
-      }
+      });
     });
 
     function removeMonthlyLabelFromAmounts() {
@@ -178,7 +72,7 @@ function run() {
       }, 150);
     }
   }
-  new MonthlySeal();
+  // new MonthlySeal();
   // Remove the loading curtain after half second
   setTimeout(() => {
     const container = document.querySelector(".peta-monthly-nudge");
